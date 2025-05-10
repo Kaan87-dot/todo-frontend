dbb00d3430e0bce96c3c34a7323ad4552af0e469
Kaan <seyhunkaan87@gmail.com>
2025-05-11 01:45:53 +0200

    M2: Vue App mit eigener Komponente und GET-Route

---
+++ src/components/TaskList.vue
@@ -0,0 +1,21 @@
+<!-- src/components/TaskList.vue -->
+<template>
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      {{ task.title }} – {{ task.done ? 'Erledigt' : 'Offen' }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: 'Einkaufen gehen', done: false },
+        { id: 2, title: 'Hausaufgaben machen', done: true }
+      ]
+    };
+  }
+};
+</script>
